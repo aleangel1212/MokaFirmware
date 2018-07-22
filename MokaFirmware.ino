aa41ece7404dbe384ac351e5bdfce061da4fe363
DylanKirke <dylankirke@gmail.com>
2018-07-22 15:15:53 -0400

    safety push cause storm, loop updates and helper functions

--- MokaFirmware.ino
+++ MokaFirmware.ino
@@ -18,6 +18,17 @@
 #define PUMP2 9
 #define PUMP3 10
 
+/* --- Relay ---*/
+#define RELAY 999
+
+/* --- Base Water Times --- */
+//TODO FOR FULL CAPACITY MEASURE ACTUAL TIMES
+#define SIX 2.66
+#define EIGHT 3.55
+#define TEN  4.44
+#define TWELVE 5.32
+
+
 /* --- RFID --- */
 //PN532_I2C pn532_i2c(Wire);
 //NfcAdapter nfc = NfcAdapter(pn532_i2c);
@@ -35,6 +46,14 @@ int status = WL_IDLE_STATUS;
 
 
 /*----- Helper Functions -----*/
+void turnOnRelay() {
+  digitalWrite(RELAY, HIGH)
+}
+
+void turnOffRelay() {
+  digitalWrite(RELAY, LOW)
+}
+
 void printWifiStatus() {
   Serial.print("SSID: ");
   Serial.println(WiFi.SSID());
@@ -46,7 +65,7 @@ void printWifiStatus() {
 
 JsonObject& get(String route) {
   StaticJsonBuffer<200> jsonBuffer;
-  
+
   client.get(route);
 
   String response = client.responseBody();
@@ -58,29 +77,64 @@ JsonObject& get(String route) {
 
 void step(int m, int steps) {
   int motors[] = { MOTOR1, MOTOR2, MOTOR3 };
-    
+
   for(int x = 0; x < 25 * steps; x++) {
     digitalWrite(motors[m-1],HIGH);
     delay(10);
     digitalWrite(motors[m-1],LOW);
     delay(10);
-  } 
+  }
 }
 
 void pump(int p, double seconds) {
   int pumps[] = { PUMP1, PUMP2, PUMP3 };
-  
+
   digitalWrite(pumps[p-1], HIGH);
   delay(1000 * seconds);
   digitalWrite(pumps[p-1], LOW);
 }
 
+double calcPumpTime(int cream, int size) {
+
+  double pumpTime = 0;
+  double percentCream = (double)cream;
+  percentCream =/ 100;
+  if (size == 6)
+    pumpTime  = SIX*(1-percentCream);
+  if (size == 8)
+    pumpTime  = EIGHT*(1-percentCream);
+  if (size == 10)
+    pumpTime  = TEN*(1-percentCream);
+  if (size == 12)
+    pumpTime  = TWELVE*(1-percentCream);
+
+  return pumpTime;
+
+}
+
+int checkPayment(){
+  return 1;
+}
+
+void defaultCoffee(){
+  step(1, 2);
+  pumpTime = calcPumppumpTime(0, 8)
+  delay(2000);
+  pump(1, pumpTime);
+}
+
+void customCoffee(int type, int size, int cream){
+  step(type, 2);
+  pumpTime = calcPumppumpTime(cream, size);
+  delay(2000);
+  pump(1, pumpTime);
+}
 
 /*----- Main Functions -----*/
 void setup() {
   Serial.begin(9600);
   while(!Serial);
-  
+
   /*--- Pin Setup ---*/
   pinMode(MOTOR1, OUTPUT);
   pinMode(MOTOR2, OUTPUT);
@@ -90,6 +144,8 @@ void setup() {
   pinMode(PUMP2, OUTPUT);
   pinMode(PUMP3, OUTPUT);
 
+  pinMode(RELAY, OUTPUT);
+
   /*--- WiFi Setup ---*/
   while ( status != WL_CONNECTED) {
     Serial.println("Attempting to connect to Network named: " + (String)ssid);
@@ -97,14 +153,43 @@ void setup() {
   }
 
   printWifiStatus();
-  
+
   /*--- RFID Setup ---*/
-  //nfc.begin();
+  nfc.begin();
 }
 
 void loop() {
-  JsonObject& data = get("/coffee/36B6597B");
-  int cream = data["cream"];
-  Serial.println(cream);
+
+  String userID = "";
+
+  if(nfc.tagPresent()){
+    NfcTag tag = nfc.read();
+    userID = tag.getUidString();
+  }
+
+  if(userID != ""){
+    JsonObject& data = get("/coffee/" + userID);
+    //JsonObject& data = get("/coffee/36B6597B");
+    //TODO error check for non found user.
+    if(data != NULL){
+      int cream = data["cream"];
+      int size  = data["size"];
+      String type = data["type"];
+
+      if(checkPayment()){
+        customCoffee(type, size, cream);
+      }
+
+    }
+    else{
+      delay(5000);
+    }
+
+
+  }
+
+
+
+
   while(1);
 }
